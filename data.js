40a800fbc02db9a9375aaea3a545639c1e1b98ca
unknown <nilarjundas@outlook.com>
2020-03-20 15:29:28 +0530

    scripts

---
+++ data.js
@@ -0,0 +1,305 @@
+export const Items = [
+  {
+    id: 1,
+    title: "Learn C# By Building a Simple RPG Game",
+    img: "img/Csharp.jpg",
+    tech: ["C#"],
+    extURL: `http://scottlilly.com/learn-c-by-building-a-simple-rpg-index/`
+  },
+  {
+    id: 2,
+    title: "Create a Rogue-like game in C#",
+    img: "img/Csharp.jpg",
+    tech: ["C#"],
+    extURL: `https://roguesharp.wordpress.com/`
+  },
+  {
+    id: 3,
+    title: "Create a Blank App with C# and Xamarin",
+    img: "img/Csharp.jpg",
+    tech: ["C#", "Xamarin"],
+    extURL: `https://www.intertech.com/Blog/xamarin-tutorial-part-1-create-a-blank-app/`
+  },
+  {
+    id: 4,
+    title: "Build iOS Photo Library App with C#",
+    img: "img/Csharp.jpg",
+    tech: ["C#", "Xamarin"],
+    extURL: `https://www.raywenderlich.com/134049/building-ios-apps-with-xamarin-and-visual-studio`
+  },
+  {
+    id: 5,
+    title: "Building the CoreWiki CMS with C#",
+    img: "img/Csharp.jpg",
+    tech: ["C#", "ASP.NET", "Razor Pages"],
+    extURL: `https://www.youtube.com/playlist?list=PLVMqA0_8O85yC78I4Xj7z48ES48IQBa7p`
+  },
+  {
+    id: 6,
+    title: "Build an Interpreter with C/C++",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `http://www.craftinginterpreters.com/`
+  },
+  {
+    id: 7,
+    title: "Build Your Own Text Editor with C/C++",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `http://viewsourcecode.org/snaptoken/kilo/`
+  },
+  {
+    id: 8,
+    title: "How to Program an NES Game in C",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `https://nesdoug.com/`
+  },
+  {
+    id: 9,
+    title: "Write an OS from scratch with C/C++",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `https://github.com/tuhdo/os01`
+  },
+  {
+    id: 10,
+    title: "How to Write an Emulator with C/C++",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/`
+  },
+  {
+    id: 11,
+    title: "Game Programming with C++ & SDL",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `http://lazyfoo.net/tutorials/SDL/`
+  },
+  {
+    id: 12,
+    title: "Write Your Own Virtual Machine",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `https://justinmeiners.github.io/lc3-vm/`
+  },
+  {
+    id: 13,
+    title: "Tetris Tutorial in C++ Platform Independent",
+    img: "img/C.jpg",
+    tech: ["C/C++"],
+    extURL: `http://javilop.com/gamedev/tetris-tutorial-in-c-platform-independent-focused-in-game-logic-for-beginners/`
+  },
+  {
+    id: 14,
+    title: "Build an Interpreter in JAVA",
+    img: "img/java.jpg",
+    tech: ["Java"],
+    extURL: `http://www.craftinginterpreters.com/`
+  },
+  {
+    id: 15,
+    title: "Build a Simple HTTP Server with Java",
+    img: "img/java.jpg",
+    tech: ["Java"],
+    extURL: `http://javarevisited.blogspot.com/2015/06/how-to-create-http-server-in-java-serversocket-example.html`
+  },
+  {
+    id: 16,
+    title: "Build an Android Flashlight App",
+    img: "img/java.jpg",
+    tech: ["Java", "Android"],
+    extURL: `https://www.youtube.com/watch?v=dhWL4DC7Krs`
+  },
+  {
+    id: 17,
+    title: "Spring Boot App with User Authentication",
+    img: "img/java.jpg",
+    tech: ["Java", "Spring MVC"],
+    extURL: `https://scotch.io/tutorials/build-a-spring-boot-app-with-user-authentication`
+  },
+
+
+    {
+      id: 18,
+      title: "Social Networking Website using React",
+      img: "img/react.jpg",
+      tech: ["React", "NodeJS", "Firebase"],
+      extURL: `https://www.youtube.com/watch?v=m_u6P5k0vP0`
+    },
+    {
+      id: 19,
+      title: "E-Commerce Website using React",
+      img: "img/react.jpg",
+      tech: ["React"],
+      extURL: `https://www.youtube.com/watch?v=-edmQKcOW8s`
+    },
+    {
+      id: 20,
+      title: "Create a Job Board using React",
+      img: "img/react.jpg",
+      tech: ["React", "NodeJS", "Cron"],
+      extURL: `https://www.youtube.com/watch?v=lauywdXKEXI`
+    },
+    {
+      id: 21,
+      title: "Make Breakout game in C++",
+      img: "img/C.jpg",
+      tech: ["C/C++", "OpenGL"],
+      extURL: `https://learnopengl.com/In-Practice/2D-Game/Breakout`
+    },
+    {
+      id: 22,
+      title: "Spotify Inspired Web Design",
+      img: "img/design.jpg",
+      tech: ["HTML", "CSS"],
+      extURL: `https://www.youtube.com/watch?v=RZ-Oe4_Ew7g`
+    },
+    {
+      id: 23,
+      title: "Build a Reddit Bot using Python",
+      img: "img/python.jpg",
+      tech: ["Python"],
+      extURL: `https://www.pythonforengineers.com/build-a-reddit-bot-part-1`
+    },
+    {
+      id: 24,
+      title: "Create a Simple Python Text Editor",
+      img: "img/python.jpg",
+      tech: ["Python"],
+      extURL: `https://www.instructables.com/id/Create-a-Simple-Python-Text-Editor`
+    },
+    {
+      id: 25,
+      title: "A BitTorrent client in Python",
+      img: "img/python.jpg",
+      tech: ["Python"],
+      extURL: `https://markuseliasson.se/article/bittorrent-in-python`
+    },
+    {
+      id: 26,
+      title: "Barcode & QR Code Scanner using Python",
+      img: "img/python.jpg",
+      tech: ["Python", "OpenCV"],
+      extURL: `https://www.pyimagesearch.com/2018/05/21/an-opencv-barcode-and-qr-code-scanner-with-zbar`
+    },
+    {
+      id: 27,
+      title: "Build Game of Life using Python",
+      img: "img/python.jpg",
+      tech: ["Python"],
+      extURL: `https://robertheaton.com/2018/07/20/project-2-game-of-life`
+    },
+    {
+      id: 28,
+      title: "Build a Netflix Landing Page Clone",
+      img: "img/design.jpg",
+      tech: ["HTML", "CSS", "JavaScript"],
+      extURL: `https://www.youtube.com/watch?v=P7t13SGytRk`
+    },
+    {
+      id: 29,
+      title: "Simple AI Chatbot With Speech API",
+      img: "img/nodejs.jpg",
+      tech: ["NodeJS", "JavaScript"],
+      extURL: `https://www.smashingmagazine.com/2017/08/ai-chatbot-web-speech-api-node-js`
+    },
+    {
+      id: 30,
+      title: "Responsive Grid CSS Website Layout",
+      img: "img/design.jpg",
+      tech: ["HTML", "CSS"],
+      extURL: `https://www.youtube.com/watch?v=moBhzSC455o`
+    },
+    {
+      id: 31,
+      title: "A BitTorrent Client in Node.js",
+      img: "img/nodejs.jpg",
+      tech: ["NodeJS", "JavaScript"],
+      extURL: `https://allenkim67.github.io/programming/2016/05/04/how-to-make-your-own-bittorrent-client.html`
+    },
+    {
+      id: 32,
+      title: "Build A Job Scraping Web App",
+      img: "img/nodejs.jpg",
+      tech: ["NodeJS", "JavaScript"],
+      extURL: `https://www.freecodecamp.org/news/how-i-built-a-job-scraping-web-app-using-node-js-and-indreed-7fbba124bbdc`
+    },
+    {
+      id: 33,
+      title: "Writing a Blockchain in Node.js",
+      img: "img/nodejs.jpg",
+      tech: ["NodeJS", "JavaScript", "Blockchain"],
+      extURL: `https://www.jsmonday.dev/articles/34/writing-a-blockchain-in-node-js`
+    },
+    {
+      id: 34,
+      title: "Intro to Blockchain with Python",
+      img: "img/python.jpg",
+      tech: ["Python", "Blockchain"],
+      extURL: `http://adilmoujahid.com/posts/2018/03/intro-blockchain-bitcoin-python`
+    },
+    {
+      id: 35,
+      title: "Make a Voxel Engine using C/C++",
+      img: "img/C.jpg",
+      tech: ["C/C++", "OpenGL"],
+      extURL: `https://sites.google.com/site/letsmakeavoxelengine/home`
+    },
+    {
+      id: 36,
+      title: "Intro to JavaScript on the server, and APIs",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript"],
+      extURL: `https://medium.com/thebit/intro-to-javascript-on-the-server-and-apis-8a117f937360`
+    },
+    {
+      id: 37,
+      title: "Animated Book Store with JavaScript",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript", "jQuery", "CSS"],
+      extURL: `https://uxplanet.org/designing-animated-book-store-with-javascript-jquery-and-css-f2f7d3335d29`
+    },
+    {
+      id: 38,
+      title: "Build URL Shortener with JavaScript",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript"],
+      extURL: `https://www.freecodecamp.org/news/building-a-simple-url-shortener-with-just-html-and-javascript-6ea1ecda308c/`
+    },
+    {
+      id: 39,
+      title: "Build a Todo List App with JavaScript",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript"],
+      extURL: `https://freshman.tech/todo-list`
+    },
+    {
+      id: 40,
+      title: "JavaScript Animations with Anime.js",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript"],
+      extURL: `https://medium.com/@ajmeyghani/creating-javascript-animations-with-anime-js-f2b14716cdc6`
+    },
+    {
+      id: 41,
+      title: "Build a Simple Gantt Chart with JavaScript",
+      img: "img/javascript.jpg",
+      tech: ["JavaScript", "CSS"],
+      extURL: `https://webdesign.tutsplus.com/tutorials/build-a-simple-gantt-chart-with-css-and-javascript--cms-33813`
+    },
+    {
+      id: 42,
+      title: "Microsoft Homepage Clone",
+      img: "img/design.jpg",
+      tech: ["HTML", "CSS", "JavaScript"],
+      extURL: `https://www.youtube.com/watch?v=uKgn-To1C4Q`
+    },
+    {
+      id: 43,
+      title: "Whatsapp Clone using React Native",
+      img: "img/react.jpg",
+      tech: ["React Native", "NodeJS", "GraphQL"],
+      extURL: `https://medium.com/react-native-training/building-chatty-a-whatsapp-clone-with-react-native-and-apollo-part-1-setup-68a02f7e11`
+    }
+  ];
