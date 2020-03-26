9f254a95f8791c1fc56a54b98761f7d68a1e9b7e
unknown <nilarjundas@outlook.com>
2020-03-26 11:38:53 +0530

    update description

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Categories from "../src/components/Landing/Categories";
 import Footer from "../src/components/Landing/Footer";
 
 export default class Landing extends Component {
-
   render() {
     return (
       <div>
@@ -14,14 +13,14 @@ export default class Landing extends Component {
           <meta name="ProjectLearn" content="Learn Code By Doing Projects" />
           <meta
             name="description"
-            content="Tutorials are great, but building projects is the best way to learn. Do project based learning and learn code the right way!"
+            content="A project-based learning approach in web development, mobile development, game development, machine learning and artificial intelligence. Learn code the right way!"
           />
           <meta
             name="keywords"
-            content="project, tutorial, learn code by doing, project based learning, learn code free"
+            content="project, tutorial, learn code by doing, project based learning, learn code free, web development, app development, game development, machine learning, artificial intelligence"
           />
           {/* <!--Title--> */}
-          <title>ProjectLearn - Learn Code By Doing Projects</title>
+          <title>ProjectLearn - Learn to Code by Creating Projects</title>
         </Head>
         <div>
           <Navbar />
