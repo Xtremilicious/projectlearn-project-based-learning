e8720851540636e3872e96274eff87ca48510513
unknown <nilarjundas@outlook.com>
2020-07-04 13:34:50 +0530

    testing readme

--- src/utils/generate_readme.js
+++ src/utils/generate_readme.js
@@ -23,7 +23,7 @@ To contribute to this list, head over to [CONTRIBUTE.md](https://github.com/Xtre
 
   fileContents = fileContents.concat(`
 ### Web Development: \n
-| Project | Technologies | Link |
+| Project &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; | Technologies | Link |
 | :-------------- |:-|:----|
 `);
 
