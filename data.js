fe3d21ebf1c50250385c7666505167611756bc31
Nilarjun Das <nilarjundas@outlook.com>
2021-04-06 21:45:04 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ exports.projectsData = [
     type: "video",
     title: "Build a Super Mario themed Google map with JavaScript",
     category: ["web-dev"],
-    tech: ["JavaScript, PHP, Node, Python, React, Ruby, Angular"],
+    tech: ["JavaScript", "PHP", "Node", "Python", "React", "Ruby", "Angular"],
     datePublished: "March 01, 2021",
     projectURL: "https://www.youtube.com/watch?v=CdDXbvBFXLY"
   },
