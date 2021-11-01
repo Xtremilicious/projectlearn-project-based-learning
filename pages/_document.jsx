ae49276dedb3b5b4f5e748c859851510837b626f
Nilarjun Das <nilarjundas@outlook.com>
2021-11-01 20:17:06 +0530

    fix script link

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -53,18 +53,7 @@ export default class MyDocument extends Document {
             async={true}
             src="https://app.appzi.io/bootstrap/bundle.js?token=OQNTh"
           />
-          <script
-            data-name="BMC-Widget"
-            data-cfasync="false"
-            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-            data-id=""
-            data-description="Support me on Buy me a coffee!"
-            data-message=""
-            data-color="#FFDD00"
-            data-position="Right"
-            data-x_margin="18"
-            data-y_margin="18"
-          ></script>
+         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="xtremilicious" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 
           {this.props.styleTags}
         </Head>
