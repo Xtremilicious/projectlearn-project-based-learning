246858f50b6212918086052da05cda9aa2027226
Nilarjun Das <nilarjundas@outlook.com>
2024-05-13 18:31:34 +0530

    tracking (#85)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,8 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -57,10 +58,32 @@ export default class MyDocument extends Document {
             async={true}
             src="https://app.appzi.io/bootstrap/bundle.js?token=OQNTh"
           />
+
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P9CT8SN8');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
         </Head>
         <body>
           <Main />
           <NextScript />
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P9CT8SN8"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
         </body>
       </Html>
     );
