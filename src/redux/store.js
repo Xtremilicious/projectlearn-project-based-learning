1a3ef1abc1ae223882e87db19f415ef8199061c8
Nilarjun Das <nilarjundas@outlook.com>
2024-05-13 12:11:44 +0530

    remove comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import dataReducer from './reducers/dataReducer';
 
-// Define the initial state
 const initialState = {};
 
-// Create a rootReducer by combining all reducers
 const rootReducer = combineReducers({
   data: dataReducer
 });
 
-// Create the Redux store
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
