ed82c9ce8c2d741b4e04f58c6c8d5b713efed656
Nilarjun Das <nilarjun@outlook.com>
2025-02-10 02:31:42 +0530

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -59,6 +59,12 @@ export default class MyDocument extends Document {
             src="https://app.appzi.io/bootstrap/bundle.js?token=OQNTh"
           />
 
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2095098316338345"
+            crossOrigin="anonymous"
+          ></script>
+
           {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
